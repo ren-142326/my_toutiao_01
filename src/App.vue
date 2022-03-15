7935c594157128c3ea55eab0b2f0202dc465876b
renjinwei <renjinwei@cpms.com.cn>
2022-03-15 19:17:43 +0800

    [^]1

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,15 @@
     <van-cell-group>
       <van-cell title="单元格" value="内容" />
       <van-cell title="单元格" value="内容" label="描述信息" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
     </van-cell-group>
   </div>
 </template>
