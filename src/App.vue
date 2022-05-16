705fc1a8488b82df6b79f945c0b8b57f81b8af4a
renjingwei <2909001175@qq.com>
2022-05-17 02:14:03 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,21 @@
       <van-cell title="单元格" value="内容" label="描述信息" />
       <van-cell title="单元格" value="内容" label="描述信息" />
       <van-cell title="单元格" value="内容" label="描述信息" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
     </van-cell-group>
   </div>
 </template>
