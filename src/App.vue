ae24435b2e0f11bfa0549ef4e133a96934dd9d42
renjingwei <2909001175@qq.com>
2022-05-17 02:38:51 +0800

    test ssh

--- src/App.vue
+++ src/App.vue
@@ -13,35 +13,6 @@
     <van-cell-group>
       <van-cell title="单元格" value="内容" />
       <van-cell title="单元格" value="内容" label="描述信息" />
-      <van-cell title="单元格" value="内容" label="描述信息" />
-      <van-cell title="单元格" value="内容" label="描述信息" />
-      <van-cell title="单元格" value="内容" label="描述信息" />
-      <van-cell title="单元格" value="内容" label="描述信息" />
-      <van-cell title="单元格" value="内容" label="描述信息" />
-      <van-cell title="单元格" value="内容" label="描述信息" />
-      <van-cell title="单元格" value="内容" label="描述信息" />
-      <van-cell title="单元格" value="内容" label="描述信息" />
-      <van-cell title="单元格" value="内容" label="描述信息" />
-      <van-cell title="单元格" value="内容" label="描述信息" />
-      <van-cell title="单元格" value="内容" label="描述信息" />
-      <van-cell title="单元格" value="内容" label="描述信息" />
-      <van-cell title="单元格" value="内容" label="描述信息" />
-      <van-cell title="单元格" value="内容" label="描述信息" />
-      <van-cell title="单元格" value="内容" label="描述信息" />
-      <van-cell title="单元格" value="内容" label="描述信息" />
-      <van-cell title="单元格" value="内容" label="描述信息" />
-      <van-cell title="单元格" value="内容" label="描述信息" />
-      <van-cell title="单元格" value="内容" label="描述信息" />
-      <van-cell title="单元格" value="内容" label="描述信息" />
-      <van-cell title="单元格" value="内容" label="描述信息" />
-      <van-cell title="单元格" value="内容" label="描述信息" />
-      <van-cell title="单元格" value="内容" label="描述信息" />
-      <van-cell title="单元格" value="内容" label="描述信息" />
-      <van-cell title="单元格" value="内容" label="描述信息" />
-      <van-cell title="单元格" value="内容" label="描述信息" />
-      <van-cell title="单元格" value="内容" label="描述信息" />
-      <van-cell title="单元格" value="内容" label="描述信息" />
-      <van-cell title="单元格" value="内容" label="描述信息" />
     </van-cell-group>
   </div>
 </template>
